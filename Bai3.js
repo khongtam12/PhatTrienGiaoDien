56a401eb548ac176ab9d5dc40a54020238cfe63f
H82M19\Student <Student>
2025-01-08 19:59:39 +0700

    finish JSF Part1 - Question3

---
+++ Bai3.js
@@ -0,0 +1,31 @@
+var diemTbCaHeo1=(96+108+89)/3
+var diemTbGauTui1=(88+91+100)/3
+function Nguoichienthang(diemtb1,diemtb2) {
+    if(diemtb1>diemtb2 && diemtb1>=100)
+    console.log("Đội Cá heo giành cúp");
+    else if(diemtb1<diemtb2 && diemtb2>=100)
+    console.log("Đội Gấu túi giành cúp");
+
+    else if(diemtb1==diemtb2 && diemtb1>=100){
+        console.log("cả 2 đội đều giành cúp");
+    }
+    else
+        console.log("cả 2 đội không giành được cúp");
+ 
+
+}
+var diemTbCaHeo1=(96+108+89)/3
+var diemTbGauTui1=(88+91+100)/3
+console.log(diemTbCaHeo1);
+console.log(diemTbGauTui1);
+Nguoichienthang(diemTbCaHeo1,diemTbGauTui1)
+var diemTbCaHeo2=(97+112+101)/3
+var diemTbGauTui2=(109+95+123)/3
+console.log(diemTbCaHeo2);
+console.log(diemTbGauTui2);
+Nguoichienthang(diemTbCaHeo2,diemTbGauTui2)
+var diemTbCaHeo3=(97+112+101)/3
+var diemTbGauTui3=(109+95+106)/3
+console.log(diemTbCaHeo3);
+console.log(diemTbGauTui3);
+Nguoichienthang(diemTbCaHeo3,diemTbGauTui3)
