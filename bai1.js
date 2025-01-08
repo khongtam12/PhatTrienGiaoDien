12e2d5cfa44f7980bcad0950de0311be9fc1d5ff
H82M19\Student <Student>
2025-01-08 19:19:36 +0700

    finish JSF part 1 - question 1

---
+++ bai1.js
@@ -0,0 +1,11 @@
+var massMark=78
+var heightMark=1.69
+var massJohn=95
+var heightJohn=1.88
+var BMIMark=massMark/(heightMark*heightMark)
+
+var BMIJohn=massJohn/(heightJohn*heightJohn)
+var markHigher=BMIMark>BMIJohn
+console.log(BMIMark);
+console.log(BMIJohn);
+console.log(markHigher);
